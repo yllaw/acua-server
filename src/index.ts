4f7ab7dd5097e2d152efb6f7e63283b2eb53cd7a
fgudino3 <frankyjg96@aol.com>
2019-04-17 10:36:53 -0700

    prepared for ibm cloud deployment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import {AcuaApplication} from './application';
-import {ApplicationConfig} from '@loopback/core';
+import { AcuaApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
+const cfenv = require('cfenv');
+const appEnv = cfenv.getAppEnv();
 
-export {AcuaApplication};
+export { AcuaApplication };
 
 export async function main(options: ApplicationConfig = {}) {
+  // Set the port assined for the app
+  if (!options) options = {};
+  if (!options.rest) options.rest = {};
+  options.rest.port = appEnv.isLocal ? options.rest.port : appEnv.port;
+  options.rest.host = appEnv.isLocal ? options.rest.host : appEnv.host;
+
   const app = new AcuaApplication(options);
   await app.boot();
   await app.start();
