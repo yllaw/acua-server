cb3fbcd2f95b29602ca8f98a1f595d7590f2bb50
Marco Argentieri <marco.argentieri@hotmail.com>
2019-04-22 23:02:02 -0700

    init animal controller

---
+++ src/controllers/animal.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getFilterSchemaFor,
+  getWhereSchemaFor,
+  patch,
+  put,
+  del,
+  requestBody,
+} from '@loopback/rest';
+
+export class AnimalController{
+  
+}
